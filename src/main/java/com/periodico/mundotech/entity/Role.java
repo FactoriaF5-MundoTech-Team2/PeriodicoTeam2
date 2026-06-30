2d56c7c7767d2f9dfce0764afb35315a1fb1bd68
nagicome03 <nayeligiselle.0206@gmail.com>
2026-06-30 15:26:35 +0200

    Role table created

--- src/main/java/com/periodico/mundotech/entity/Role.java
+++ src/main/java/com/periodico/mundotech/entity/Role.java
@@ -1,26 +1,43 @@
 package com.periodico.mundotech.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 
 @Entity
-@Table(name = "roles")
-@Getter
-@Setter
+@Table(name = "role")
+@Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @Builder.Default
+    @JsonIgnore
+    private Set<User> users = new HashSet<>();
 
 }
