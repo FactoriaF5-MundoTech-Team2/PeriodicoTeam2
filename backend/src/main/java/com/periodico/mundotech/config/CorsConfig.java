953c33d7aa25e1ebad2552a02cd062e365429188
alvi103-png <alvi103@gmail.com>
2026-07-06 13:43:23 +0200

    refactor: move backend code into backend file

---
+++ backend/src/main/java/com/periodico/mundotech/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.periodico.mundotech.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("**")
+                .allowCredentials(true);
+    }
+}
---
+++ backend/src/main/java/com/periodico/mundotech/dto/response/UserResponseDTO.java
@@ -0,0 +1,5 @@
+package com.periodico.mundotech.dto.response;
+
+public class UserResponseDTO {
+
+}
